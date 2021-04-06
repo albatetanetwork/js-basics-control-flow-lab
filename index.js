b147afcd132697ea840ddcdb07290625f35d7480
Alba <albatetanetwork@gmail.com>
2021-04-06 00:32:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+     } 
+   else if (1999 < feet && feet < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+     }
+   else {
+    result = 'No can do.';
+   }
+    return result;
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city == `NYC`) {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default :
+      return "Bye."  
+  }
 }
